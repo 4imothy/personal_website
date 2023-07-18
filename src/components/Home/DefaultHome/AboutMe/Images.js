b569c7347aa51fdb9959c3e5004fa32f85a0becb
Timothy Cronin <tcroniniv@gmail.com>
2023-07-18 12:04:25 -0400

    update images

--- src/components/Home/DefaultHome/AboutMe/Images.js
+++ src/components/Home/DefaultHome/AboutMe/Images.js
@@ -1,36 +1,69 @@
 import { Parallax } from 'react-scroll-parallax';
 import styles from './Images.module.scss';
-import shoeOutlook from '../../AboutMeImages/ShoesOutlook.jpg';
-import blueWater from '../../AboutMeImages/BlueWater.jpg';
-import meInCrevice from '../../AboutMeImages/MeInCrevice.jpg';
-import sunset from '../../AboutMeImages/Sunset.jpg';
-import meOnBridge from '../../AboutMeImages/MeOnBridge.jpg'
+import asheville from '../../AboutMeImages/asheville.jpeg';
+import fish from '../../AboutMeImages/fish.jpeg';
+import frisbee from '../../AboutMeImages/frisbee.jpeg';
+import tennis from '../../AboutMeImages/tennis.jpeg';
 
 const Images = () => {
     return (
         <div className={styles.root}>
-            <Parallax className={styles.imageContainer} rotateZ={[-30, 180]} translateX={[60, -100]} easing={[.35, .34, 1, .08]}>
-                <img className={styles.image} src={meOnBridge} alt='cool nature shot' />
+            <Parallax
+                className={styles.imageContainer}
+                rotateZ={[55, -180]}
+                translateX={[60, -110]}
+                easing={[0.35, 0.34, 1, 0.08]}
+            >
+                <img
+                    style={{ width: '20%', marginTop: '-35%' }}
+                    className={styles.image}
+                    src={asheville}
+                    alt="cool nature shot"
+                />
             </Parallax>
 
-            <Parallax className={styles.imageContainer} rotateZ={[55, -180]} translateX={[70, -110]} easing={[.35, .34, 1, .08]}>
-                <img className={styles.waterImage} src={blueWater} alt='cool nature shot' />
+            <Parallax
+                className={styles.imageContainer}
+                rotateZ={[-30, 180]}
+                translateX={[60, -100]}
+                easing={[0.35, 0.34, 1, 0.08]}
+            >
+                <img
+                    style={{ width: '15%', marginTop: '-15%' }}
+                    className={styles.image}
+                    src={fish}
+                    alt="cool nature shot"
+                />
             </Parallax>
 
-            <Parallax className={styles.imageContainer} translateX={[75, -300]} rotate={[-30,300]} easing={[.35, .34, 1, .08]}>
-                <img className={styles.creviceImage} src={meInCrevice} alt='cool nature shot' />
+            <Parallax
+                className={styles.imageContainer}
+                translateX={[70, -100]}
+                rotate={[-30, 300]}
+                easing={[0.35, 0.34, 1, 0.08]}
+            >
+                <img
+                    style={{ width: '8%', marginTop: '5%' }}
+                    className={styles.image}
+                    src={frisbee}
+                    alt="cool nature shot"
+                />
             </Parallax>
-
-            <Parallax className={styles.imageContainer} translateX={[58, -180]} rotate={[-30, 70]} easing={[.35, .34, 1, .08]}>
-                <img className={styles.sunset} src={sunset} alt='cool nature shot' />
-            </Parallax>
-
-            <Parallax className={styles.imageContainer} translateX={[90, -180]} rotate={[-20, 90]}easing={[.35, .34, 1, .08]}>
-                <img className={styles.flower} src={shoeOutlook} alt='cool nature shot' />
+            <Parallax
+                className={styles.imageContainer}
+                rotateZ={[30, -200]}
+                translateX={[80, -150]}
+                easing={[0.35, 0.34, 1, 0.08]}
+            >
+                <img
+                    style={{ width: '10%', marginTop: '5%' }}
+                    className={styles.image}
+                    src={tennis}
+                    alt="cool nature shot"
+                />
             </Parallax>
         </div>
-
     );
-}
+};
 
 export default Images;
--- src/components/Home/SmallHome/AboutMe/AboutMePhotos.js
+++ src/components/Home/SmallHome/AboutMe/AboutMePhotos.js
@@ -1,31 +1,48 @@
-import BlueWater from '../../AboutMeImages/BlueWater.jpg';
-import MeInCrevice from '../../AboutMeImages/MeInCrevice.jpg';
-import MeOnBridge from '../../AboutMeImages/MeOnBridge.jpg';
-import Sunset from '../../AboutMeImages/Sunset.jpg';
+import asheville from '../../AboutMeImages/asheville.jpeg';
+import fish from '../../AboutMeImages/fish.jpeg';
+import frisbee from '../../AboutMeImages/frisbee.jpeg';
+import tennis from '../../AboutMeImages/tennis.jpeg';
 
 const AboutMePhotos = () => {
     return (
-        <div style={{
-            width: '80%',
-            marginLeft: '10%', 
-            display: 'flex',
-            flexWrap: 'wrap',
-        }}>
-            <img style={{
-                width: '50%',
+        <div
+            style={{
+                width: '80%',
+                marginLeft: '10%',
+                display: 'flex',
+                flexWrap: 'wrap',
             }}
-                src={MeOnBridge} alt="Bridge" />
-            <img style={{
-                width: '50%'
-            }} src={BlueWater} alt="Blue Water" />
-            <img style={{
-                width: '50%'
-            }} src={Sunset} alt="Sunset" />
-            <img style={{
-               width: '50%'
-            }} src={MeInCrevice} alt="In Rocks" />
+        >
+            <img
+                style={{
+                    width: '50%',
+                }}
+                src={asheville}
+                alt="Bridge"
+            />
+            <img
+                style={{
+                    width: '50%',
+                }}
+                src={frisbee}
+                alt="Blue Water"
+            />
+            <img
+                style={{
+                    width: '50%',
+                }}
+                src={tennis}
+                alt="Sunset"
+            />
+            <img
+                style={{
+                    width: '50%',
+                }}
+                src={fish}
+                alt="In Rocks"
+            />
         </div>
     );
-}
+};
 
 export default AboutMePhotos;
